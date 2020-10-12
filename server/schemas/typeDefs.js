0129849227db88c6c95a748d1a06fc0098633101
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-10-12 14:47:34 -0500

    working on resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,39 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth 
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: String!): User
+        removeBook(bookId: String!): User
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [string]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
+`;
+
+module.exports = typeDefs;
